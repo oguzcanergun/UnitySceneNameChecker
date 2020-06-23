e26e06036d0af8efe689951082a5c29428c43873
Oguzcan Ergun <oguzcanergun@gmail.com>
2020-06-23 23:02:08 +0300

    Script file added
    Base script file added. (Nothing much to say about this)

---
+++ CheckSceneScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OguzcanErgun
+{
+    namespace CheckSceneName
+    {
+        public class CheckSceneScript : MonoBehaviour
+        {
+            [SerializeField] private string sceneName;
+
+            // Update is called once per frame
+            void Update()
+            {
+                if (Input.GetKey(KeyCode.Space))//this is here to simulate desired event trigger
+                {
+                    LoadANewScene();
+                }
+            }
+
+            public void LoadANewScene()
+            {
+                if (CheckSceneName())
+                {
+                    SceneManager.LoadScene(sceneName);
+                }
+                else
+                {
+                    Debug.Log("Problem with the scene name");
+                }
+            }
+
+            bool CheckSceneName()
+            {
+                int sceneCount = SceneManager.sceneCountInBuildSettings;//get number of scenes in build settings.
+                if (sceneCount > 0)
+                {
+                    for (int i = 0; i < sceneCount; i++)
+                    {
+                        string sceneInBuild = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));//get name of i indexed scene name in build settings parsed out from its file address.
+                        if (string.Compare(sceneName, sceneInBuild) == 0)//compare desired scene name with i indexed scene name in build settings.
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
